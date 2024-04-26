0b05eb056b1389b955800ec409b734df2debe3e0
Katharina Blaimschein <katharina_blaimschein@aon.at>
2024-04-26 11:28:56 +0200

    Changed Project type.

---
+++ telephoneBook_RDF/telephoneBook_RDF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace telephoneBook_RDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
